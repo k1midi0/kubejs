3c09ab6dabd0dc94018c51530236bc0d89694fbc
铃狸 <207581392@qq.com>
2026-01-07 18:24:51 +0800

    添加弹药盒合成路径

--- server_scripts/example.js
+++ server_scripts/example.js
@@ -2,5 +2,29 @@
 
 // Visit the wiki for more info - https://kubejs.com/
 
-console.info('Hello, World! (Loaded server scripts)')
+ServerEvents.recipes(event =>{
+    event.remove({mod:"tacz"})
+    const create = event.recipes.create
+    event.shaped(
+        "tacz:target",
+        [
+            " r ",
+            " t ",
+            " g "
+        ],
+        {
+            r: "minecraft:redstone",
+            t: "create:iron_sheet",
+            g: "minecraft:stick"
+        }
+    )
+    create.compacting(Item.of('tacz:ammo_box', '{Level:0}'),
+        Item.of("create:iron_sheet",2))
+    create.compacting(Item.of('tacz:ammo_box', '{Level:1}'),
+        [Item.of("create:iron_sheet",2),Item.of('create_new_age:overcharged_golden_sheet',2)])
+    create.compacting(Item.of('tacz:ammo_box', '{Level:2}'),
+        [Item.of("create:iron_sheet",2),Item.of('create_new_age:overcharged_golden_sheet',2),Item.of("create_new_age:overcharged_diamond",2)])
+
+        
+})
 
