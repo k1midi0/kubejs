5519cddb97845e8e365d30e924586560862ec217
Xiaohaozi <2726393348@qq.com>
2026-01-10 20:59:47 +0800

    CLF10-中折式霰弹枪配方(采用连续组装）

--- server_scripts/example.js
+++ server_scripts/example.js
@@ -119,6 +119,27 @@ ServerEvents.recipes(event => {
         }
     )
 
+    create.sequenced_assembly(//CLF10-中折式霰弹枪配方草案
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:1,GunFireMode:"SEMI",GunId:"qkl:clf10",HasBulletInBarrel:1b}'),
+        "minecraft:iron_ingot",//输入铁锭
+        [
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","minecraft:iron_ingot"]),
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","minecraft:iron_ingot"]),//组装两个铁锭作为机匣基材
+        create.cutting("minecraft:iron_ingot","minecraft:iron_ingot"),//切割机匣基材
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","create:andesite_alloy"]),
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","create:andesite_alloy"]),//组装两个安山合金作为机匣粗胚
+        create.cutting("minecraft:iron_ingot","minecraft:iron_ingot"),//切割机匣粗胚
+        create.pressing("minecraft:iron_ingot","minecraft:iron_ingot"),//锻压为机匣
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","minecraft:lightning_rod"]),//组装撞针
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","create:cardboard_sword"]),
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","create:cardboard_sword"]),//组装铁管作为枪管（暂时用纸管代替）
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","#minecraft:stripped"]),
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","#minecraft:stripped"]),
+        create.deploying("minecraft:iron_ingot",["minecraft:iron_ingot","#minecraft:stripped"]),
+        create.cutting("minecraft:iron_ingot","minecraft:iron_ingot"),//组装去皮木材，切割枪托、护木和握把
+        ]
+    )
+
     create.sequenced_assembly(//刺刀配方
         Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:30,GunFireMode:"SEMI",GunId:"qkl:m1905",HasBulletInBarrel:1b}'),
         "create:iron_sheet",//输入铁板
@@ -130,23 +151,6 @@ ServerEvents.recipes(event => {
     ).transitionalItem("create:iron_sheet").loops(1)//中间物为铁板，循环1次
 
 })
-
-    event.shaped(//CLF10-中折式霰弹枪配方草案
-        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:1,GunFireMode:"SEMI",GunId:"qkl:clf10",HasBulletInBarrel:1b}'),
-        [
-            "   TG",
-            "   GB",
-            " DGB ",
-            "MM   "
-        ],
-        {
-            G: "create:cardboard_sword",//暂时用纸管代替铁管
-            T: "",//铁粒
-            D: "",//铁板
-            B: "minecraft:stick",//木棍
-            M: "minecraft:stripped_oak_wood"//木头
-        }
-    )
 
 // =========================别管下面这坨==============================
 
