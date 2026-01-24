6113f10b4d24e4a443de45f1f48817f621bf74cd
Xiaohaozi <2726393348@qq.com>
2026-01-24 19:53:15 +0800

    机匣粗胚配方补丁1，添加了经典改进的快捷方式VI

--- server_scripts/gun/gun parts/gun parts.js
+++ server_scripts/gun/gun parts/gun parts.js
@@ -47,14 +47,14 @@
 
 
 ServerEvents.recipes(event => {
-    const create = event.recipes.create//创建一个快捷方式来使用Create的配方方法
-
+    const create = event.recipes.create//创建一个快捷方式来使用机械动力（Create）的配方方法
+    const VI = event.recipes.vintageimprovements//创建一个快捷方式来使用经典改进(vintageimprovements）的配方方法
     create.cutting(//铁板切出小铁板
         Item.of("kubejs:iron_sheet_small", 2),
         "create:iron_sheet"
     )
 
-    
+
     VI.curving(
         'kubejs:rough_casing',  // 输出物品
         'minecraft:iron_ingot' // 输入物品
